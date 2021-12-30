f5f22a15ede99561e570e7cb1800367640fed261
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2021-12-29 23:48:08 -0600

    back from holidays completed mobile sites, working on +501px

--- resume.js
+++ resume.js
@@ -8,10 +8,22 @@
     let button = document.getElementById("btn");
     button.addEventListener('click', listener1);
 
+
 let screenTop = function (){
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     }
     let buttonTop = document.getElementById("topBtn")
     buttonTop.addEventListener('click',screenTop);
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            buttonTop.style.display = "block";
+        } else {
+            buttonTop.style.display = "none";
+        }
+    }
+
 })();
